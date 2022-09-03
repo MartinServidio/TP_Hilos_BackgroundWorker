30dafc577905c8f6c9d5ad24ea164db42a3b07cd
Martin Servidio <86500202+MartinServidio@users.noreply.github.com>
2022-09-03 17:45:04 -0300

    Add files via upload

--- TP_Hilos_Background/TP_Hilos_Background/Form1.vb
+++ TP_Hilos_Background/TP_Hilos_Background/Form1.vb
@@ -6,14 +6,17 @@ Imports System.dll
 Public Class Form1
     Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
         Dim i As Integer
-        For i = 1 To 2000
-            BackgroundWorker1.WorkerReportsProgress = True
-            BackgroundWorker1.ReportProgress(i / 10)
+        Dim maximo As Integer = Val(TextBox1.Text)
+        For i = 1 To maximo
+            'Dim info As Byte() = New UTF8Encoding(True).GetBytes("Linea " + Str(i) + vbCrLf)
+            'fs.Write(info, 0, info.Length)
+            My.Computer.FileSystem.WriteAllText(Application.StartupPath & "\INFORME.TXT", "LINEA: " & i & vbCrLf, True)
 
-            Threading.Thread.Sleep(100)
+            ' ACTUALIZA LA BARRA DE PROGRESO
+            BackgroundWorker1.ReportProgress(CInt(100 * i / CInt(maximo)))
         Next
-
     End Sub
+
     Private Sub BackgroundWorker1_ProgressChanged(ByVal sender As Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
         If e.ProgressPercentage <= Val(TextBox1.Text) Then
             ProgressBar1.Value = e.ProgressPercentage
@@ -26,23 +29,8 @@ Public Class Form1
         If Val(TextBox1.Text) > 0 Then
             Button2.Visible = False
             TextBox1.Enabled = False
+            BackgroundWorker1.WorkerReportsProgress = True
             BackgroundWorker1.RunWorkerAsync()
-
-            Dim maximo As Integer = Val(TextBox1.Text)
-            ProgressBar1.Maximum = maximo
-
-            Dim path As String = "C:\Users\PC1\Desktop\Hilos.txt"
-            Dim fs As FileStream = File.Create(path)
-
-            For i = 1 To maximo Step 1
-                Dim info As Byte() = New UTF8Encoding(True).GetBytes("Linea " + Str(i) + vbCrLf)
-                fs.Write(info, 0, info.Length)
-            Next
-
-            fs.Close()
-
-        Else
-            MsgBox("Inserte cantidad maxima de lineas de texto")
         End If
     End Sub
 End Class
